fe9f4a8cbbd57ca43ececa521c94b148d432fa94
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2019-07-12 15:27:27 -0500

    submitAnswer function worked on
    submitAnswer function worked on. Can't do more work until startQuiz is done for testing.

--- index.js
+++ index.js
@@ -10,15 +10,40 @@ function startQuiz() {
 
 function submitAnswer() {
     console.log("submitting answer");
-    //on submit answer button
-    //remove current body
-    //collect answer
-    //compare answer against actual answer for questionNum
-    //delete current body
-    //if answer was correct, correctAnswers++ and set wasAnswerSubmitedCorrect = true;
-    //else, do not incriment correctAnswer and set wasAnswerSubmittedCorrect = false;
-    //replace body with feedback page
-}
+    //on answerSubmitButton pressed
+    $('#answer-selector-form').submit(function(e) {
+        e.preventDefault();
+        //collect answer
+        let selectedAnswer = $('input:checked');
+        console.log(selectedAnswer);
+        selectedAnswer = selectedAnswer.val();
+        //let correctAnswer = answer[questionNum];
+        console.log(selectedAnswer);
+        //compare answer against actual answer for questionNum
+        if (selectedAnswer === correctAnswer) {
+            wasAnswerSubmitedCorrect = true;
+            NumOfCorrectAnswers++;
+        }
+        else if (selectedAnswer !== correctAnswer) {
+            wasAnswerSubmitedCorrect = false;
+        }
+        else {
+            console.log("Something broke at Line 22 of index.js");
+        }
+        //remove current body
+        $("#answer-section").remove();
+        $("question-text").remove();
+        //replace body with feedback page
+        $("body").append(`
+            <section>
+                <h2 id="answer-result">${answerResult}</h2>
+                <h3 id="correct-answer">${correctAnswer}</h3>
+                <p id="additional-info">${additionalInfo}</p>
+                <button id="next-question-button">Next Question</button>
+                <img src="${feedbackImage}" alt="ALT INFO">
+            </section>
+        `);
+})}
 
 function nextQuestion() {
     console.log("moving to next question")
