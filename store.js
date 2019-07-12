01b860ee44e86d1fc732af7554e44aa81a2b7d00
Ramarduk <ramarduk@gmail.com>
2019-07-12 13:13:44 -0600

    Completed store.js

--- store.js
+++ store.js
@@ -12,7 +12,7 @@ const STORE = [
     additionalInfo: 'George Lucas had the idea for Star Wars as early as 1971, and would release "Star Wars" on May 25th, 1977.'
   },
   {
-    question: ' Who plays the character of Luke Skywalker in A New Hope?',
+    question: 'Who plays the character of Luke Skywalker in A New Hope?',
     answers: [
       'Mark Hamill',
       'Toby Maguire',
@@ -21,5 +21,98 @@ const STORE = [
     ],
     correctAnswer: 'Mark Hamill',
     additionalInfo: 'Mark Hamill has also done voicework for The Joker in many types of DC media.'
+    
   },
-  
+  {
+    question: ' How much did it cost for Disney to buy the rights to the Star Wars franchise?',
+    answers: [
+      '$6.2 Million',
+      '$24 Billion',
+      '$4.05 Billion',
+      '$97.85 Million'
+    ],
+    correctAnswer: '$4.05 billion',
+    additionalInfo: 'Disney aquired the overall rights to Star Wars in 2012. It took 6 years for Star Wars to earn that money back.'
+  },
+  {
+    question: 'Which one of the following is not a type of Lightsaber form?',
+    answers: [
+      'Ataru',
+      'Teras Kasi',
+      'Juyo',
+      'Shii-Cho'
+    ],
+    correctAnswer: 'Teras Kasi',
+    additionalInfo: 'Teras Kasi is a style of hand to hand combat in the Star Wars universe, not a lightsaber form.'
+  },
+  {
+    question: 'Who is Darth Sidious?',
+    answers: [
+      'Sheev Palpatine',
+      'Count Dooku',
+      'Asajj Ventress',
+      'Savage Opress'
+    ],
+    correctAnswer: 'Sheev Palpatine',
+    additionalInfo: 'Sheev Palpatine was Darth Sidious even before the beginning of The Phantom Menace and had been working to infiltrate the Republic Senate for his own gains.'
+  },
+  {
+    question: 'Ahsoka Tano started wielding 2 lightsabers at a time in The Clone Wars Season 3. What colors were her sabers at this time?',
+    answers: [
+      'Green and Blue',
+      'Both Green',
+      'Green and Yellow',
+      'Both Blue'
+    ],
+    correctAnswer: 'Green and Yellow',
+    additionalInfo: 'Ahsoka would later go on to have 2 White lightsabers, as shown in Star Wars Rebels and the novel Ahsoka by E. K. Johnston.'
+  },
+  {
+    question: ' While there is a canonical reason for Mace Windu\'s purple lightsaber, what\'s the real life reason he had a purple lightsaber?',
+    answers: [
+      'Mace Windu was supposed to be special from the get go.',
+      'They got bored of using just Green, Blue, and Red sabers in animation.',
+      'Sam L. Jackson asked for it.',
+      'It was a rendering error that they ended up keeping.'
+    ],
+    correctAnswer: 'Sam L. Jackson asked for it.',
+    additionalInfo: ' "We had this big arena, this fight scene with all these Jedi and they’re fightin’ or whatever. And I was like...I wanna be able to find myself in this big ol’ scene. So I said to George, "You think maybe I can get a purple lightsaber?" - Samuel L Jackson.'
+  },
+  {
+    question: 'The Rule of 2 states that there should only ever be 2 Sith: a master and an apprentice. Who invented this rule?',
+    answers: [
+      'Mace Windu was supposed to be special from the get go.',
+      'They got bored of using just Green, Blue, and Red sabers in animation.',
+      'Sam L. Jackson asked for it.',
+      'It was a rendering error that they ended up keeping.'
+    ],
+    correctAnswer: 'Darth Bane',
+    additionalInfo: ' "Two there should be. No more, no less. One to embody power, the other to crave it." -Darth Bane.'
+  },
+  {
+    question: 'The famous cantina song used in multiple types of Star Wars media is known as what genre of music?',
+    answers: [
+      'Jizz',
+      'Jazz',
+      'Folk',
+      'Swing-bop'
+      
+    ],
+    correctAnswer: 'Jizz',
+    additionalInfo:' "Jizz was a genre of music. Jizz-wailers were musicians who specialized in the genre." -Wookieepedia'
+    
+  },
+  {
+    question: 'What was Anakin Skywalker\'s medichlorian count during The Phantom Menace?',
+    answers: [
+      'Over 350,000',
+      'Over 1 million',
+      'Over 60,000',
+      'Over 20,000'
+      
+    ],
+    correctAnswer: 'Over 20,000',
+    additionalInfo:'"Even Master Yoda doesn\'t have a midi-chlorian count that high!" -Obi-Wan Kenobi, The Phantom Menace'
+    
+  }
+];
