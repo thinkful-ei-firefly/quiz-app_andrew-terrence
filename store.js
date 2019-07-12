341c0294e8cf159f97d3c1234aa3d8e902f94729
Ramarduk <ramarduk@gmail.com>
2019-07-12 12:32:24 -0600

    Starting Store.js

--- store.js
+++ store.js
@@ -1,1 +1,25 @@
 'use strict';
+const STORE = [
+  {
+    question: 'Who created Star Wars?',
+    answers: [
+      'Luke SkyWalker',
+      'JJ Abrams',
+      'Rian Johnson',
+      'George Lucas'
+    ],
+    correctAnswer: 'George Lucas',
+    additionalInfo: 'George Lucas had the idea for Star Wars as early as 1971, and would release "Star Wars" on May 25th, 1977.'
+  },
+  {
+    question: ' Who plays the character of Luke Skywalker in A New Hope?',
+    answers: [
+      'Mark Hamill',
+      'Toby Maguire',
+      'Hayden Christianson',
+      'Harrison Ford'
+    ],
+    correctAnswer: 'Mark Hamill',
+    additionalInfo: 'Mark Hamill has also done voicework for The Joker in many types of DC media.'
+  },
+  
