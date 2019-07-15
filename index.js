5b0f6c2ef883f6ecc37e4b8c5089bcf327cd85e7
Ramarduk <ramarduk@gmail.com>
2019-07-15 14:00:36 -0600

    more aria changes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 'use strict';
 
 function startQuiz() {
@@ -7,7 +8,7 @@ function startQuiz() {
     let currentQuestion = STORE.questions[0];
     STORE.numOfCorrectAnswers = 0;
     $('body').html(`
-            <header>
+            <header role='banner'>
               <h1>Star Wars Quiz</h1>
               <h1 id=score>${STORE.numOfCorrectAnswers}/10</h1>
             </header> </br>     
@@ -64,7 +65,7 @@ function submitAnswer() {
     <h1 id=score>${STORE.numOfCorrectAnswers}/10</h1>
     </header> </br>
             <section aria-label='Feedback section' id="feedback-section">
-                <h2 id="answer-result">You got Question ${STORE.numOfCurrentQuestion+1} ${answerResult}</h2>
+                <h2 id="answer-result">${answerResult}</h2>
                 <h3 id="correct-answer">${currentQuestion.correctAnswer}</h3>
                 <p id="additional-info">${currentQuestion.additionalInfo}</p>
                 <button type=button id="next-question-button">Next Question</button>
@@ -85,7 +86,7 @@ function nextQuestion() {
       let currentQuestion = STORE.questions[STORE.numOfCurrentQuestion];
       $('#feedback-section').remove();
       $('body').html(`
-      <header>
+      <header role='banner'>
         <h1>Star Wars Quiz</h1>
         <h1 id=score>${STORE.numOfCorrectAnswers}/10</h1>
       </header> </br>      
@@ -123,13 +124,9 @@ function nextQuestion() {
 function displayResults(){
   if(STORE.numOfCorrectAnswers===10){
     $('body').html(
-      `<header>
-      <h1>Star Wars Quiz</h1>
-      </header>
-      <section>
-      <h2>Your total score is ${STORE.numOfCorrectAnswers}/${STORE.questions.length}</h2>
-      </section>
+      `<h1>Star Wars Quiz</h1>
       <section aria-label='Quiz results' class='results'>
+      <h2>Your total score is ${STORE.numOfCorrectAnswers}/${STORE.questions.length}</h2>
       <img src='assets/fargreater.jpg' alt='You have become a far greater Jedi than I could ever hope to be.'>
     </section>
     <button id='start-quiz'>Retry</button>`); 
@@ -137,44 +134,32 @@ function displayResults(){
   }
   else if(STORE.numOfCorrectAnswers >=5 && STORE.numOfCorrectAnswers < 10){
     $('body').html(
-      `<header>
-      <h1>Star Wars Quiz</h1>
-      </header>
-      <section>
-      <h2>Your total score is ${STORE.numOfCorrectAnswers}/${STORE.questions.length}</h2>
-      </section>
+      `<h1>Star Wars Quiz</h1>
       <section aria-label='Quiz results' class='results'>
+      <h2>Your total score is ${STORE.numOfCorrectAnswers}/${STORE.questions.length}</h2>
       <img src='assets/greatkid.jpg' alt='Great, Kid. Don't get cocky.'>
-      <button id='start-quiz'>Retry</button>
-      </section>`); 
+    </section>
+    <button id='start-quiz'>Retry</button>`); 
   }
   else if(STORE.numOfCorrectAnswers>=1 && STORE.numOfCorrectAnswers<5) {
     $('body').html(
-      `<header>
-      <h1>Star Wars Quiz</h1>
-      </header>
-      <section>
-      </section>
+      `<h1>Star Wars Quiz</h1>
       <section aria-label='Quiz results' class='results'>
       <h2>Your total score is ${STORE.numOfCorrectAnswers}/${STORE.questions.length}</h2>
       <img src='assets/ihavefailedyou.jpg' alt='I have failed you, Anakin.'>
-      <button id='start-quiz'>Retry</button>
-      </section>`); 
+    </section>
+    <button id='start-quiz'>Retry</button>`); 
   }
   else {
     $('body').html(
-      `<header>
-      <h1>Star Wars Quiz</h1>
-      </header>
-      <section>
-      <h2>Your total score is ${STORE.numOfCorrectAnswers}/${STORE.questions.length}</h2>
-      </section>
+      `<h1>Star Wars Quiz</h1>
       <section aria-label='Quiz results' class='results'>
+      <h2>Your total score is ${STORE.numOfCorrectAnswers}/${STORE.questions.length}</h2>
       <img src='assets/amazing.jpg' alt='Amazing. Every word you said, was wrong.'>
-      <button id='start-quiz'>Retry</button>
-      </section>`); 
+    </section>
+    <button id='start-quiz'>Retry</button>`); 
   }
-  $('body').append(`    <footer>
+  $('body').append(`    <footer role='contentinfo'>
   <p>Quiz created by Andrew Jessen-Tyler and Terrence Harvey.</p>
   <p>Images, story, setting, and other attributes of Star Wars are owned entirely by the copyright holders and are protected under Fair Use for educational purposes in this quiz.</p>
 </footer>`);
